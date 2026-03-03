b0f4f279c795fb44d3b90effd85a6c7e32486e4c
labsfiap <pedrocastrosilva50@gmail.com>
2026-03-03 20:50:57 -0300

    Fim da aula dia 03/03

--- src/Mar03/Ex2/RmNotaMediaMetodo.java
+++ src/Mar03/Ex2/RmNotaMediaMetodo.java
@@ -10,10 +10,20 @@ public class RmNotaMediaMetodo {
         int[] rm = new int[N];
         double[] nota1 = new double[N];
         double[] nota2 = new double[N];
+        int[] aprovados = new int[N];
         int n = entradaDados(rm, nota1, nota2);
+
+        double[] media = new double[n];
+        calcularMedia(n, media, nota1, nota2);
         for (int i = 0; i < n; i++){
-            System.out.println(rm[i]);
+            System.out.println("\nRM: " + rm[i] + "\nMédia: " + media[i] + "\n");
         }
+        int nAp = geraListaAprovados(n, media, rm, aprovados);
+            System.out.println("\n\t ****** Lista de aprovados ******");
+            for (int i = 0; i < nAp; i++) {
+                System.out.println(" \n                 RM:" +aprovados[i]);
+        }
+
     }
     public static int entradaDados(int[] rm, double[] nota1, double[] nota2) {
         Scanner sc = new Scanner(System.in);
@@ -32,6 +42,21 @@ public class RmNotaMediaMetodo {
         return n;
     }
 
+    public static void calcularMedia(int n, double[] media, double[] nota1, double[] nota2){
+        for (int i = 0; i < n; i++){
+            media[i] = (nota1[i] + nota2[i])/2;
+        }
+    }
 
+    public static int geraListaAprovados(int n, double[] media, int[] rm, int[] aprovados) {
+        int nAp = 0;
+        for (int i = 0; i < n; i++) {
+            if (media[i] >= 6) {
+                aprovados[nAp] = rm[i];
+                nAp++;
+            }
+        }
+        return nAp;
+    }
 
 }
