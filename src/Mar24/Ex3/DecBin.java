de31358604826cfa0593af93394787d07b6df737
labsfiap <pedrocastrosilva50@gmail.com>
2026-03-31 19:43:41 -0300

    finalização ex 3 DecBin

--- src/Mar24/Ex3/DecBin.java
+++ src/Mar24/Ex3/DecBin.java
@@ -1,12 +1,23 @@
 package Mar24.Ex3;
 
+import Mar24.pilhas.PilhasInt;
+
 import java.util.Scanner;
 
 public class DecBin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        PilhasInt pilha = new PilhasInt();
+        pilha.init();
         int decimalValue, resto;
         System.out.print("Valor em decimal: ");
         decimalValue = sc.nextInt();
+        while (decimalValue != 0){
+            resto = decimalValue % 2;
+            pilha.push(resto);
+            decimalValue = decimalValue/2;
+        }
+        System.out.println("Valor em binário: ");
+        pilha.popEsvazia();
     }
 }
